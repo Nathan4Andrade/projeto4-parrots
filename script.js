113e0d9c12454d955820068e6e0284adf883ff61
Nathan Andrade <nathan.henriques@ccc.ufcg.edu.br>
2023-04-07 15:45:17 -0300

    script do jogo funcionando

--- script.js
+++ script.js
@@ -1,36 +1,67 @@
 let parrots = [
-  "../img/bobrossparrot.gif",
-  "../img/explodyparrot.gif",
-  "../img/fiestaparrot.gif",
-  "../img/metalparrot.gif",
-  "../img/revertitparrot.gif",
-  "../img/tripletsparrot.gif",
-  "../img/unicornparrot.gif",
+  "./img/bobrossparrot.gif",
+  "./img/explodyparrot.gif",
+  "./img/fiestaparrot.gif",
+  "./img/metalparrot.gif",
+  "./img/revertitparrot.gif",
+  "./img/tripletsparrot.gif",
+  "./img/unicornparrot.gif",
 ];
 
 let qtdCards = 0;
 const deckOfCards = [];
 let move = [];
 const ulCards = document.querySelector(".cards");
+let counter = 0;
+let timer = 0;
+let interval = 0;
+
+randomDeck();
+startGame();
+displayCards(qtdCards);
+
+makePairs();
+randomCards();
+renderCards();
+interval = setInterval(startsTimer, 1000);
 
 function startGame() {
   qtdCards = prompt("Com quantas cartas você quer jogar?");
   while (qtdCards % 2 !== 0 || qtdCards < 4 || qtdCards > 14) {
     qtdCards = prompt("Com quantas cartas você quer jogar?");
   }
+}
 
-  displayCards(qtdCards);
-  for (let i = 0; i < qtdCards; i++) {
-    ulCards.innerHTML += `<li onClick="clickCards(this)" class="card"> 
+function renderCards() {
+  for (let i in deckOfCards) {
+    const card = deckOfCards[i];
+    ulCards.innerHTML += card;
+  }
+}
+
+function createCard(index) {
+  const parrot = parrots[index];
+  const cardHTML = `<li onClick="clickCards(this)" class="card"> 
                                <div class="front-face face">
-                                    <img data-test="face-down-image" src="../img/back.png" alt="">
+                                    <img data-test="face-down-image" src="./img/back.png" alt="">
                                 </div>
                                 <div class="back-face face">
-                                    <img data-test="face-up-image" src="${parrots[i]}" alt="">
+                                    <img data-test="face-up-image" src="${parrot}" alt="${parrot}">
                                 </div>
                                 
                             </li>`;
+  return cardHTML;
+}
+
+function makePairs() {
+  const quantityOfPairs = qtdCards / 2;
+  for (let i = 0; i < quantityOfPairs; i++) {
+    const card = createCard(i);
+    deckOfCards.push(card);
+    deckOfCards.push(card);
   }
+
+  return deckOfCards;
 }
 
 function displayCards(qtdCards) {
@@ -53,17 +84,17 @@ function comparator() {
   return Math.random() - 0.5;
 }
 
-function shuffleCards() {
+function randomCards() {
   return deckOfCards.sort(comparator);
 }
 
-function shuffleInitialVariables() {
+function randomDeck() {
   return parrots.sort(comparator);
 }
 
 function clickCards(clickedCard) {
   if (isAValidCard(clickedCard)) {
-    clickedCard.classList.toggle("flipped");
+    clickedCard.classList.add("flipped");
     move.push(clickedCard);
 
     if (move.length === 2) {
@@ -80,4 +111,64 @@ function isAValidCard(card) {
   return !flipped || !right;
 }
 
-startGame();
+function preventsAdditionalClicks() {
+  document.querySelector(".cards").classList.add("no-click");
+}
+
+function allowClicks() {
+  document.querySelector(".cards").classList.remove("no-click");
+}
+
+function checksIdenticalsCards() {
+  const firstMove = move[0];
+  const secondMove = move[1];
+
+  if (firstMove.innerHTML === secondMove.innerHTML) {
+    firstMove.classList.add("right");
+    secondMove.classList.add("right");
+    console.log("igual");
+    setTimeout(checkTheEndOfTheGame, 500);
+  } else {
+    setTimeout(turnCardToInitialPosition, 1000);
+  }
+  counter++;
+}
+
+function turnCardToInitialPosition() {
+  move.forEach((element) => {
+    element.classList.remove("flipped");
+  });
+  allowClicks();
+  move = [];
+}
+
+function checkTheEndOfTheGame() {
+  const numberOfFlippedCards = document.querySelectorAll(".right").length;
+  const successMessage = `Você ganhou em ${counter} jogadas! A duração do jogo foi de ${timer} segundos`;
+
+  if (qtdCards == numberOfFlippedCards) {
+    clearInterval(interval);
+    alert(successMessage);
+    restartGame();
+  } else {
+    allowClicks();
+    move = [];
+  }
+}
+
+function restartGame() {
+  const restart = prompt("Você gostaria de reiniciar a partida?");
+
+  if (restart === "sim") {
+    window.location.reload(true);
+  } else if (restart === "não") {
+    console.log("jogo terminado");
+  } else {
+    restartGame();
+  }
+}
+
+function startsTimer() {
+  timer++;
+  document.querySelector(".timer").innerHTML = timer;
+}
