b27beb1b21a60cf443c571eff19b0b1f617ff43b
Nathan Andrade <nathan.henriques@ccc.ufcg.edu.br>
2023-04-07 12:19:33 -0300

    layout basico

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
-let qtdCards = prompt("Com quantas cartas você quer jogar?");
-while (qtdCards % 2 !== 0 || qtdCards < 4 || qtdCards > 14) {
+let PARROTS = [
+  "bobrossparrot",
+  "explodyparrot",
+  "fiestaparrot",
+  "metalparrot",
+  "revertitparrot",
+  "tripletsparrot",
+  "unicornparrot",
+];
+
+let qtdCards = 0;
+const cards = [];
+const ulCards = document.querySelector(".cards");
+
+function startGame() {
   qtdCards = prompt("Com quantas cartas você quer jogar?");
+  while (qtdCards % 2 !== 0 || qtdCards < 4 || qtdCards > 14) {
+    qtdCards = prompt("Com quantas cartas você quer jogar?");
+  }
+
+  displayCards(qtdCards);
+  for (let i = 0; i < qtdCards; i++) {
+    ulCards.innerHTML += `<li onClick="clickCard" class="card"></li>`;
+  }
 }
 
-const cards = [qtdCards];
+function displayCards(qtdCards) {
+  if (qtdCards == 4) {
+    ulCards.classList.add("four");
+  } else if (qtdCards == 6) {
+    ulCards.classList.add("six");
+  } else if (qtdCards == 8) {
+    ulCards.classList.add("eight");
+  } else if (qtdCards == 10) {
+    ulCards.classList.add("ten");
+  } else if (qtdCards == 12) {
+    ulCards.classList.add("twelve");
+  } else if (qtdCards == 14) {
+    ulCards.classList.add("fourteen");
+  }
+}
 
-const teste = document.querySelector(".teste");
-
-teste.innerHTML = qtdCards;
+startGame();
